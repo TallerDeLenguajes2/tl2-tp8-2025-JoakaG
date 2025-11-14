9a0ad502e5de7093783fdfdc51dd1a41d77bd0c6
JoakaG <girbaujoaquina2@gmail.com>
2025-11-14 11:33:33 -0300

    no se

--- ViewModels/AgregarProductoViewModel.cs
+++ ViewModels/AgregarProductoViewModel.cs
@@ -3,5 +3,13 @@ public class AgregarProductoViewModel
     int IdPresupuesto{get; set;}
     List<Producto> Productos {get; set;}
     double MontoTotal {get; set;}
-    
+
+    public AgregarProductoViewModel(int IdPresupuesto, List<Producto> Productos, double MontoTotal)
+    {
+        this.IdPresupuesto = IdPresupuesto;
+        this.Productos = Productos;
+        this.MontoTotal = MontoTotal;
+    }
+
+
 }
