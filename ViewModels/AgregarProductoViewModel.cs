edfed61a28eee23e8fce43e936dfaa2c34ac091b
JoakaG <girbaujoaquina2@gmail.com>
2025-11-14 11:47:33 -0300

    casita

--- ViewModels/AgregarProductoViewModel.cs
+++ ViewModels/AgregarProductoViewModel.cs
@@ -1,15 +1,18 @@
 public class AgregarProductoViewModel
 {
-    int IdPresupuesto{get; set;}
-    List<Producto> Productos {get; set;}
-    double MontoTotal {get; set;}
+    public int IdPresupuesto{get; set;}
+    public List<Producto> Productos {get; set;}
+    public Producto Producto {get; set;}
+    public int Cantidad {get; set;}
+    public double MontoTotal {get; set;}
 
-    public AgregarProductoViewModel(int IdPresupuesto, List<Producto> Productos, double MontoTotal)
+    public AgregarProductoViewModel(int IdPresupuesto, List<Producto> Productos, double MontoTotal, Producto Producto, int Cantidad)
     {
         this.IdPresupuesto = IdPresupuesto;
         this.Productos = Productos;
         this.MontoTotal = MontoTotal;
+        this.Producto = Producto;
+        this.Cantidad = Cantidad;
     }
-
 
 }
